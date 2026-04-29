daedbaf1bbc070a3b1c3944216af586958ea0abb
sgy011 <2139424961@qq.com>
2026-04-29 11:58:26 +0800

    Delete start.bat

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul 2>&1
-title TransVsverter 开发服务
-echo ========================================
-echo   TransVsverter - 一键启动
-echo ========================================
-echo.
-python start_dev.py %*
-pause
